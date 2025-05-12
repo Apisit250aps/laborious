e858e768ec4052f2878fabc2f8c867631a9533a1
YUapisit <aps.apisit250@gmail.com>
2025-05-12 19:45:29 +0700

    Add MongoDB client setup and update dependencies for authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import NextAuth from 'next-auth'
+import Google from 'next-auth/providers/google'
+import client from './client'
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [Google],
+  session: { strategy: 'jwt', maxAge: 1000 * 60 * 60 * 24 * 30 }
 })
