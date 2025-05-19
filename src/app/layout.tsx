fd8d7b3dbc8c47dc3d5e6de47302ed74493a6168
YUapisit <aps.apisit250@gmail.com>
2025-05-19 14:46:51 +0700

    Refactor layout and globals styles to use Mitr font; streamline font management and enhance typography consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mitr } from "next/font/google";
 import "./globals.css";
 import 'remixicon/fonts/remixicon.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const mitr = Mitr({
+  variable: "--font-mitr",
+  subsets: ["latin", "thai"],
+  weight: ["200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mitr.variable} antialiased`}
       >
         {children}
       </body>
